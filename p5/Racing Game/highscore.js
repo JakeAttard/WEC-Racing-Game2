6d52a1225eeb922f82e19caedecf383e949d6365
Jake Attard <jakeattard18@gmail.com>
2018-09-28 01:11:01 +1000

    Bug fixes & changes

--- p5/Racing Game/highscore.js
+++ p5/Racing Game/highscore.js
@@ -1,55 +1,55 @@
 function setup() {
-	  createCanvas(1200, 800);
-    
-    
-	  headDivHScore = createDiv('High score'); 
-	  headDivHScore.position(50, 50);
+    createCanvas(1200, 800);
+
+
+    headDivHScore = createDiv('High score');
+    headDivHScore.position(50, 50);
     headDivHScore.class('heading');
 
-    headDivName = createDiv('Name'); 
-	  headDivName.position(150, 50);
+    headDivName = createDiv('Name');
+    headDivName.position(150, 50);
     headDivName.class('heading');
 
     headDivScore = createDiv('Score');
     headDivScore.position(200, 50);
     headDivScore.class('heading');
-      
-    headDivTime = createDiv('Time'); 
-	  headDivTime.position(250, 50);
+
+    headDivTime = createDiv('Time');
+    headDivTime.position(250, 50);
     headDivTime.class('heading');
 
-	  aTag = createA('index.html', 'back to Game');
-	  aTag.position(550, height/2 + 50);
-	  aTag.class('btn btn-primary');
+    aTag = createA('index.html', 'back to Game');
+    aTag.position(550, height / 2 + 50);
+    aTag.class('btn btn-primary');
 
-	  loadJSON("scores.json", drawData);
-	}
+    loadJSON("scores.json", drawData);
+}
 
-	function drawData(data) {
-        background(100);
-		var startY = 60;
-		var startX = 70;
-        
-		for (var i=0; i<data.highscores.length; i++) {
-		  startY += 30;
-		  startX = 60;
-		  valDivHScore = createDiv(data.highscores[i].highscore); 
-		  valDivHScore.position(startX, startY);
-      valDivHScore.class('val');
+function drawData(data) {
+    background(100);
+    var startY = 60;
+    var startX = 70;
 
-		  valDivName= createDiv(data.highscores[i].name); 
-		  valDivName.position(startX + 90, startY);
-      valDivName.class('val');
+    for (var i = 0; i < data.highscores.length; i++) {
+        startY += 30;
+        startX = 60;
+        valDivHScore = createDiv(data.highscores[i].highscore);
+        valDivHScore.position(startX, startY);
+        valDivHScore.class('val');
 
-		  valDivScore = createDiv(data.highscores[i].score); 
-		  valDivScore.position(startX + 150, startY);
-      valDivScore.class('val');
+        valDivName = createDiv(data.highscores[i].name);
+        valDivName.position(startX + 90, startY);
+        valDivName.class('val');
 
-		  valDivTime = createDiv(data.highscores[i].time); 
-		  valDivTime.position(startX + 200, startY);
-      valDivTime.class('val');	
+        valDivScore = createDiv(data.highscores[i].score);
+        valDivScore.position(startX + 150, startY);
+        valDivScore.class('val');
 
-		  console.log(data.highscores[i]);
-		}
-        
-	}	
+        valDivTime = createDiv(data.highscores[i].time);
+        valDivTime.position(startX + 200, startY);
+        valDivTime.class('val');
+
+        console.log(data.highscores[i]);
+    }
+
+}
--- p5/Racing Game/sketch.js
+++ p5/Racing Game/sketch.js
@@ -37,13 +37,13 @@ var carsOvertaken = 0;
 var carRepairLives = 3;
 
 function preload() {
-    
+
     // Menu Sprites
     personImages[0] = loadImage('images/person1.png');
     personImages[1] = loadImage('images/person2.png');
     personImages[2] = loadImage('images/person3.png');
     personImages[3] = loadImage('images/person4.png');
-    
+
     officialImages[0] = loadImage('images/official1.png');
     officialImages[1] = loadImage('images/official2.png');
     officialImages[2] = loadImage('images/official3.png');
@@ -52,14 +52,14 @@ function preload() {
     officialImages[5] = loadImage('images/official6.png');
     officialImages[6] = loadImage('images/official7.png');
     officialImages[7] = loadImage('images/official8.png');
-    
+
     pitCrewImages[0] = loadImage('images/pitcrew1.png');
     pitCrewImages[1] = loadImage('images/pitcrew2.png');
     pitCrewImages[2] = loadImage('images/pitcrew3.png');
-    
+
     // Car Sound
     mySound = loadSound('sounds/racecar.mp3');
-    
+
     // Car Images
     wecPorsche = loadImage('images/Porsche.png');
     wecMcdonalds = loadImage('images/wecMcdonalds.png');
@@ -68,12 +68,12 @@ function preload() {
     carRepaired = loadImage('images/CarRepair.png');
     tyres = loadImage('images/tyres.png');
     font = loadFont('Rabbit-Hole.ttf');
-    
+
     // Car Loading Screen Images
     porscheLoadingScreenImg = loadImage('images/PorscheLoadingScreen.png');
     mcdonaldsLoadingScreenImg = loadImage('images/wecMcdonaldsLoadingScreen.png');
     audiLoadingScreenImg = loadImage('images/wecAudiLoadingScreen.png');
-    
+
     // RaceTrack Assets
     startLineLoadingScreenImg = loadImage('images/startline.png');
     raceStartFlagMenu = loadImage('images/racestartflag.png');
@@ -82,19 +82,19 @@ function preload() {
 
 function setup() {
     createCanvas(1200, 800);
-    
+
     playerSprite = createSprite(200, 400, 50, 50);
     playerSprite.addAnimation('run', personImages[0], personImages[1], personImages[2], personImages[3]);
-    playerSprite.setVelocity(1,0);
-    
+    playerSprite.setVelocity(1, 0);
+
     officialSprite = createSprite(0, 150, 50, 50);
     officialSprite.addAnimation('run', officialImages[0], officialImages[1], officialImages[2], officialImages[3], officialImages[4], officialImages[5], officialImages[6], officialImages[7]);
-    officialSprite.setVelocity(1,0);
-    
+    officialSprite.setVelocity(1, 0);
+
     pitCrewSprite = createSprite(0, 700, 50, 50);
     pitCrewSprite.addAnimation('run', pitCrewImages[0], pitCrewImages[1], pitCrewImages[2]);
-    pitCrewSprite.setVelocity(1,0);
-    
+    pitCrewSprite.setVelocity(1, 0);
+
     mySound.setVolume(0.1);
     mySound.play();
 
@@ -104,43 +104,40 @@ function setup() {
     driver = new Driver();
 
     aTag = createA('score.html', 'View High scores');
-    aTag.position(550, height/2 + 50);
+    aTag.position(550, height / 2 + 50);
     aTag.class('btn btn-primary');
     aTag.hide();
 }
 
 function draw() {
     background(100);
-    if (currentState == LOADING){
+    if (currentState == LOADING) {
         drawLoadingScreen();
-    }
-    else if (currentState == MAIN_MENU){
+    } else if (currentState == MAIN_MENU) {
         drawMainMenuScreen();
-    }
-    else if (currentState == PLAY){
+    } else if (currentState == PLAY) {
         drawPlayScreen();
-    }
-    else if (currentState == HIGH_SCORE){
+    } else if (currentState == HIGH_SCORE) {
         drawHighScore();
-    }    
-    
+    }
+
     // Progress to Main Menu after 2s 
-    if (frameCount == 250){
+    if (frameCount == 250) {
         currentState = MAIN_MENU;
     }
-    
+
 }
 
 /*
  * Draw the loading screen
  */
-function drawLoadingScreen(){
+function drawLoadingScreen() {
     textSize(40);
     textFont(font);
     textAlign(LEFT);
     fill(255);
     text('Engines Starting...', 450, 200);
-    
+
     var tintValue = map(mouseX, 0, width, 0, 255);
     tint(255, 255, 255, 255 - tintValue);
     image(porscheLoadingScreenImg, 200, 300);
@@ -152,63 +149,59 @@ function drawLoadingScreen(){
 /*
  * Draw the main menu screen
  */
-function drawMainMenuScreen(){
+function drawMainMenuScreen() {
     // Text
     textSize(40);
     textFont(font);
     textAlign(LEFT);
     fill(255);
     text('Warm Up Your Engine! Click to quickly play.', 225, 50);
-    
+
     textSize(25);
     textFont(font);
     textAlign(LEFT);
     fill(255);
     text('Instructions:', 480, 760);
-    
+
     textSize(20);
     textFont(font);
     textAlign(LEFT);
     fill(255);
     text('Move your racing driver via mouse to the car. Move your pit crew member via keyboard left, right, up and down keys.', 200, 780);
-    
+
     image(raceStartFlagMenu, 500, 100, 150, 100);
     image(startLineLoadingScreenImg, 0, 215, 1200, 50);
     image(wecMcdonalds, 50, 275);
     image(wecAudi, 240, 325);
     image(wecPorsche, 700, 275);
     image(wecAudi, 890, 325);
-    
+
     pitCrewSprite.velocity.x = (mouseX - pitCrewSprite.position.x) * 0.2;
     pitCrewSprite.velocity.y = (mouseY - pitCrewSprite.position.y) * 0.2;
     drawSprites();
-    
+
 }
 
 function keyPressed() {
-  if (keyCode == RIGHT_ARROW) {
-    playerSprite.setSpeed(1.5, 0);
-  }
-  else if (keyCode == DOWN_ARROW) {
-    playerSprite.setSpeed(1.5, 90);
-  }
-  else if (keyCode == LEFT_ARROW) {
-    playerSprite.setSpeed(1.5, 180);
-  }
-  else if (keyCode == UP_ARROW) {
-    playerSprite.setSpeed(1.5, 270);
-  }
-  else if (key == ' ') {
-    playerSprite.setSpeed(0, 0);
-  }
-  return false;
+    if (keyCode == RIGHT_ARROW) {
+        playerSprite.setSpeed(1.5, 0);
+    } else if (keyCode == DOWN_ARROW) {
+        playerSprite.setSpeed(1.5, 90);
+    } else if (keyCode == LEFT_ARROW) {
+        playerSprite.setSpeed(1.5, 180);
+    } else if (keyCode == UP_ARROW) {
+        playerSprite.setSpeed(1.5, 270);
+    } else if (key == ' ') {
+        playerSprite.setSpeed(0, 0);
+    }
+    return false;
 }
 
 /*
  * Implement the game logic
  */
-function drawPlayScreen(){
-    
+function drawPlayScreen() {
+
     background(56, 56, 56);
 
     if (frameCount % 15 === 0) {
@@ -248,8 +241,7 @@ function drawPlayScreen(){
             // Collision Repair Life Lost
             carsOvertaken = (carsOvertaken >= 0) ? (carsOvertaken - 3) : 0;
             carRepairLives--;
-        }
-        else if (opposition[i].offscreen()) {
+        } else if (opposition[i].offscreen()) {
             opposition.splice(i, 1);
         }
     }
@@ -291,7 +283,7 @@ function drawPlayScreen(){
     textAlign(LEFT);
     fill(255);
     text('Repair Lives:', 30, 725);
-    
+
     // Game Over
     if (carRepairLives === 0) {
         noLoop();
@@ -306,18 +298,18 @@ function drawPlayScreen(){
         // Line Shape
         stroke(255);
         line(320, 420, 900, 420);
-        
+
         //Ellipse Shape
         fill(0);
         ellipse(270, 390, 55, 55);
-        
+
         fill(0);
         ellipse(950, 390, 55, 55);
     }
 }
 
-function drawHighScore(){
-   
+function drawHighScore() {
+
 }
 
 
@@ -325,7 +317,7 @@ function drawHighScore(){
 /*
  * Mouse click triggers the transition between the menu and play screens.
  */
-   
+
 // Mouse Movement
 
 function mouseMoved() {
@@ -335,14 +327,13 @@ function mouseMoved() {
     }
     if (currentMousePos > mouseX) {
         driver.turnLeft();
-    }    
-    
+    }
+
 }
 
 
-function mouseClicked(){
-    if (currentState == MAIN_MENU){
+function mouseClicked() {
+    if (currentState == MAIN_MENU) {
         currentState = PLAY;
-    } else if (currentState == PLAY){
-    }
+    } else if (currentState == PLAY) {}
 }
